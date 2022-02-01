9b42380f00b123286d6265cbe81e653e1a98c167
Florian Märkl <info@florianmaerkl.de>
2022-02-01 10:45:16 +0100

    Add basic rz-test test suite

---
+++ test/bins/types.h
@@ -0,0 +1,32 @@
+
+enum Ambassador
+{
+	AMBASSADOR_PURE = 0,
+	AMBASSADOR_REASON = 1,
+	AMBASSADOR_REVOLUTION = 2,
+	AMBASSADOR_ECHOES = 3,
+	AMBASSADOR_WALL = 4,
+	AMBASSADOR_MILLION = 1000000
+};
+
+struct Window
+{
+	const char *sunlight;
+};
+
+struct Morning
+{
+	uint32_t saved_argc;
+	char **saved_argv;
+};
+
+struct Bright
+{
+	struct Morning *morning;
+	struct Window window;
+	enum Ambassador ambassador;
+};
+
+typedef struct Bright *BrightPtr;
+typedef struct Bright BrightTypedefd;
+typedef BrightTypedefd *BrightTypedefdPtr;
